24a3a005bf62c2a86a24761e911b7ca483cbdcb2
Ashruthi <ashruthibommena043@gmail.com>
2025-02-11 23:16:52 +0530

    Make the Cart Quantity Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -60,7 +60,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
        button.addEventListener('click', () =>{
         const productId = button.dataset.productId;
 
-      let matchingItem;
+      let matchingItem; 
 
         cart.forEach((item) => {
           if(productId === item.productId){
@@ -79,6 +79,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             quantity: 1
           });
         }
+
+       let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        console.log(cartQuantity);
         console.log(cart);
        });
 
